aa6e68bee5cf7727aa4c115e8d65e3b6f33c0405
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-02-11 22:53:43 +0200

    start

--- src/components/RecipeInfo.jsx
+++ src/components/RecipeInfo.jsx
@@ -1,14 +1,1 @@
 import PropTypes from 'prop-types';
-
-export const RecipeInfo = ({ icon: Icon, text }) => {
-  return (
-    <p>
-      <Icon /> {text}
-    </p>
-  );
-};
-
-RecipeInfo.propTypes = {
-  icon: PropTypes.elementType.isRequired,
-  text: PropTypes.string.isRequired,
-};
