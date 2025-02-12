fcb2ad0c9c8690d1d7828cf8617f8053742ecc22
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-02-12 12:32:27 +0200

    start 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 export const App = () => {
   return (
-    <div>
-      <h1 className="title">Recipe List</h1>
-      {recipes.map((recipe, index) => (
-        <Recipe className="card" key={index} recipe={recipe} />
-      ))}
-    </div>
+    <></>
   );
 };
  
