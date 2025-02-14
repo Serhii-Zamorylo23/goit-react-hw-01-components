60c93bf17506c7bf425b48f8ccae2ace1391a60f
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-02-14 22:11:29 +0200

    start css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import user  from './components/Profile/user.json'
+import Profile from 'components/Profile/Profile';
+import 'components/Profile/Profile.css'
 export const App = () => {
   return (
-    <></>
+    <>
+    {user.map((user,index)=>(
+      <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} key={index}/>
+    ))}
+    </>
   );
 };
  
