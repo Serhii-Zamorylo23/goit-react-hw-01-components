0e5374ec0595c8c20a32680344b1e2fb96fda40f
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-02-20 23:59:11 +0200

    finish

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,9 +3,10 @@ import FriendListItem from "./FriendListItem";
 const Friendlist=({friends})=>{
     return(
         <ul className="friend-list">
-            {friends.map(({avatar,name,isOnline})=>{
-                <FriendListItem avatar={avatar} name={name} isOnline={isOnline}/>
-            })}
+            {friends.map(({avatar,name,isOnline,id})=>(
+                <FriendListItem avatar={avatar} name={name} isOnline={isOnline} key={id}/>
+            ))}
+            
         </ul>
     )
 }
